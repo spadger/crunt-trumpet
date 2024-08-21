9b2df1c5e59bb0e88b42ccdac7b208966d879b7a
Jon Bates <jonmbates@gmail.com>
2024-08-21 18:42:54 +0200

    Less killable, more annoying

--- app/src/main/java/info/spadger/crunttrumpet/ListenerService.kt
+++ app/src/main/java/info/spadger/crunttrumpet/ListenerService.kt
@@ -1,6 +1,5 @@
 package info.spadger.crunttrumpet
 
-import android.R
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
@@ -17,6 +16,8 @@ import com.pusher.client.channel.Channel
 import com.pusher.client.connection.ConnectionEventListener
 import com.pusher.client.connection.ConnectionState
 import com.pusher.client.connection.ConnectionStateChange
+import info.spadger.crunttrumpet.MainActivity.Companion.MESSAGE_EXTRAS_KEY
+import org.json.JSONObject
 
 class ListenerService : Service() {
     override fun onBind(p0: Intent?): IBinder? {
@@ -40,14 +41,25 @@ class ListenerService : Service() {
         channel.bind("message") { event ->
             Log.i("Pusher", "Received event with data: $event")
 
-            val uiIntent = Intent(this, MainActivity::class.java)
-                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                .putExtra(MainActivity.MESSAGE_EXTRAS_KEY, event.data)
+            val payload = JSONObject(event.data)
+            val message = payload.getString("message")
+            val numSeconds = payload.getInt("numSeconds")
 
-            startActivity(uiIntent)
+            for(i in 0..numSeconds){
+
+                val colour = if(i%2==0) Color.RED else Color.BLUE
+                val uiIntent = Intent(this, MainActivity::class.java)
+                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    .putExtra(MainActivity.MESSAGE_EXTRAS_KEY, message)
+                    .putExtra(MainActivity.COLOUR_EXTRAS_KEY, colour)
+
+                startActivity(uiIntent)
+                Thread.sleep(1000)
+            }
         }
 
-        return super.onStartCommand(intent, flags, startId)
+        super.onStartCommand(intent, flags, startId)
+        return START_STICKY
     }
 
     private fun createPusherChannel(): Channel {
@@ -87,6 +99,9 @@ class ListenerService : Service() {
             .addCategory(Intent.CATEGORY_LAUNCHER)
             .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
             .addFlags(Intent.FLAG_FROM_BACKGROUND)
+            .addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
+
+
 
         val pendingIntent = PendingIntent.getActivity(
             this.applicationContext,
@@ -108,11 +123,13 @@ class ListenerService : Service() {
         val notification = NotificationCompat.Builder(applicationContext, CHANNEL_ID)
             .setContentTitle("PuppyBoggle")
             .setContentText("I'll tell you when it's time to stop...")
-            .setSmallIcon(R.drawable.sym_def_app_icon)
+            .setSmallIcon(android.R.drawable.sym_def_app_icon)
             .setContentIntent(pendingIntent)
             .setOngoing(false)
             .setAutoCancel(true)
             .setChannelId(CHANNEL_ID)
+            .setCategory(Notification.CATEGORY_ALARM)
+            .setPriority(NotificationManager.IMPORTANCE_MAX)
             .build()
         return notification
     }
--- app/src/main/java/info/spadger/crunttrumpet/MainActivity.kt
+++ app/src/main/java/info/spadger/crunttrumpet/MainActivity.kt
@@ -1,17 +1,22 @@
 package info.spadger.crunttrumpet
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.telephony.ServiceState
 import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-
+import androidx.core.content.ContextCompat.startForegroundService
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
         var startedService = false
         const val MESSAGE_EXTRAS_KEY = "__message__"
+        const val COLOUR_EXTRAS_KEY = "__colour__"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,9 +35,25 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
 
-        if(intent.extras?.containsKey(MESSAGE_EXTRAS_KEY) == true){
-            val message = intent.extras?.getString(MESSAGE_EXTRAS_KEY)
-            findViewById<TextView>(R.id.txtMain).text = message
+        if (intent.extras?.containsKey(MESSAGE_EXTRAS_KEY) == true) {
+            val message = intent.extras?.getString(MESSAGE_EXTRAS_KEY)!!
+            val colour = intent.extras?.getInt(COLOUR_EXTRAS_KEY)!!
+            findViewById<TextView>(R.id.txtMain).apply {
+                text = message
+                setBackgroundColor(colour)
+            }
+        }
+    }
+}
+
+class StartReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+
+        Log.w("BOOT", "YEah, booting now!")
+
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+            context.startForegroundService(Intent(context, ListenerService::class.java))
         }
     }
 }
